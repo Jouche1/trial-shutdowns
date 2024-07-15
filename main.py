e1871800bd06763d011efe4a5f5e9915e486fa6c
cbushue93 <134952956+cbushue93@users.noreply.github.com>
2024-07-15 13:33:27 -0500

    add conditional formatting for action rows, and None for devices already in shut down

--- main.py
+++ main.py
@@ -8,23 +8,6 @@ from openpyxl import Workbook
 from openpyxl.styles import PatternFill
 
 
-# def add_action(row):
-#     if row['TRIAL_STATUS'] == "Expired" and row['ORDER_ID'].startswith('4E') and row['IN_CX_NETWORK'] == "Yes":
-#         if row['IS_EA_ORG'] == "Yes":
-#             return 'Sales'
-#         if 'MX' in row['PROD_CODE'] or 'MS' in row['PROD_CODE']:
-#             if row['SUPPORT_SCORE'] in ['S0' or 'S1']:
-#                 return 'Sales'
-#             else: 
-#                 "Remove & shut down"
-#         else: 
-#             return "Remove & shut down"
-#     elif row['TRIAL_STATUS'] == "Expired" and row['ORDER_ID'].startswith('4E'):
-#         if row['IN_CX_NETWORK'] == "No":
-#             return "Shut-er-down"
-#     elif row['TRIAL_STATUS'] == "Not Expired":
-#         return "Let it be"
-    
 
 def add_action(row):
     if row['TRIAL_STATUS'] == "Expired" and row['ORDER_ID'].startswith('4E'):
@@ -38,21 +21,19 @@ def add_action(row):
                     "Remove & shut down"
             else: 
                 return "Remove & shut down"
+        elif row['IN_CX_NETWORK'] == "ShutDown":
+            return "None"
         else: 
             return "Shut-er-down"
     else:
-        return "Let it be"
+        return "NO SHUTDOWN"
         
-
-
-
 
 
 def update_trial_status(row):
     if not row['ORDER_ID'].startswith('4E'):
         row['TRIAL_STATUS'] = "Purchased"
     return row
-
 
 
 
@@ -63,13 +44,13 @@ def apply_conditional_formatting(sheet, df):
             prod_code_value = row[0].value
             if prod_code_value and ('MX' in str(prod_code_value) or 'MS' in str(prod_code_value)):
                 for cell in row:
-                    cell.fill = PatternFill(start_color="FFC17A", end_color="FFC17A", fill_type="solid")
+                    cell.fill = PatternFill(start_color="B2CBDE", end_color="B2CBDE", fill_type="solid")
     
     if 'IS_EA_ORG' in df.columns:
         ea_org_col_idx = df.columns.get_loc('IS_EA_ORG') + 1
         for row in sheet.iter_rows(min_row=2, max_row=len(df) + 1, min_col=ea_org_col_idx, max_col=ea_org_col_idx):
             ea_org_value = row[0].value
-            if ea_org_value and 'Yes' in str(ea_org_value):
+            if ea_org_value and "Yes" in str(ea_org_value):
                 for cell in row:
                     cell.fill = PatternFill(start_color="FDFD96", end_color="FDFD96", fill_type="solid")
 
@@ -80,6 +61,27 @@ def apply_conditional_formatting(sheet, df):
             if sup_score_value and ('0' in str(sup_score_value) or '1' in str(sup_score_value)):
                 for cell in row:
                     cell.fill = PatternFill(start_color="FFD1DC", end_color="FFD1DC", fill_type="solid")
+
+    if 'ACTION' in df.columns: 
+        action_col_idx = df.columns.get_loc('ACTION') + 1
+        for row in sheet.iter_rows(min_row=2, max_row=len(df) + 1, min_col=action_col_idx, max_col=action_col_idx):
+            action_value = row[0].value
+            if action_value and "NO SHUTDOWN" in str(action_value):
+                for cell in row:
+                    cell.fill = PatternFill(start_color="FF483F", end_color="FF483F", fill_type="solid")
+            if action_value and "Sales" in str(action_value):
+                for cell in row:
+                    cell.fill = PatternFill(start_color="FFC17A", end_color="FFC17A", fill_type="solid")
+
+    if 'TRIAL_STATUS' in df.columns: 
+        status_col_idx = df.columns.get_loc('TRIAL_STATUS') + 1
+        for row in sheet.iter_rows(min_row=2, max_row=len(df) + 1, min_col=status_col_idx, max_col=status_col_idx):
+            status_value = row[0].value
+            if status_value and "Purchased" in str(status_value):
+                for cell in row:
+                    cell.fill = PatternFill(start_color="A7CF97", end_color="A7CF97", fill_type="solid")
+
+                    
 
     else:
         print("'PROD_CODE' column not found in DataFrame.")
